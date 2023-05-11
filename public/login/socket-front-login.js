da808cc2449558401df9564b10b02bbd543b0203
Annylys Medeiros <annylysmedeiros@hotmail.com>
2023-05-10 23:31:47 -0300

    feat: incluido pagina com chat de atendimento

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -9,36 +9,16 @@ function emitirAutenticarUsuario(dados) {
 socket.on("autenticacao_sucesso", (tokenJwt) => {
   definirCookie("tokenJwt", tokenJwt);
 
-  alert("Usuário autenticado com sucesso!");
+  // alert("Usuário autenticado com sucesso!");
   window.location.href = "/telaPrincipal/index.html";
 });
-
-// const socket = io();
-
-// function emitirAutenticarUsuario(dados) {
-//   socket.emit("autenticar_usuario", dados);
-// }
-
-// socket.on("autenticacao_sucesso", (tokenJwt) => {
-//   definirCookie("tokenJwt", tokenJwt);
-
-//   console.log("Usuário autenticado com sucesso!");
-//   window.location.href = "/";
-// });
 
 socket.on("autenticacao_erro", () => alert("Erro na autenticação."));
 socket.on("usuario_nao_encontrado", () => alert("Usuário não encontrado."));
 
-// socket.on("autenticacao_sucesso", (tokenJwt) => {
-//   definirCookie("tokenJwt", tokenJwt);
-
-//   console.log("Usuário autenticado com sucesso!");
-//   window.location.href = "/";
-// });
-// socket.on("autenticacao_erro", () => alert("Erro na autenticação."));
-// socket.on("usuario_nao_encontrado", () => alert("Usuário não encontrado."));
 
 export { emitirAutenticarUsuario };
 
 //npm run dev
 //incluir ip no mongo db
+ 
